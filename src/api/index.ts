8fcc54a731337e917d80c62d7e35839e10a84056
Luna <imlvnaa@gmail.com>
2023-11-09 17:21:04 -0500

    forgot to add the router

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
+import { v0Router } from "./v0";
 import { v1Router } from "./v1";
 
 const router = Router();
 
+router.use("/v0", v0Router);
 router.use("/v1", v1Router);
 
 export const apiRouter = router;
